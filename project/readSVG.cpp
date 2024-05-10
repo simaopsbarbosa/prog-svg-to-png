495843658c1321d02f2b9a6ec11e779eb63274df
Simão Barbosa <117586175+simaopsbarbosa@users.noreply.github.com>
2024-05-10 15:46:26 +0100

    desenhar circulos e linhas

--- project/readSVG.cpp
+++ project/readSVG.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include "SVGElements.hpp"
 #include "external/tinyxml2/tinyxml2.h"
 #include <string.h>
@@ -21,21 +22,28 @@ namespace svg
         dimensions.x = xml_elem->IntAttribute("width");
         dimensions.y = xml_elem->IntAttribute("height");
         
-        // TODO complete code -->
-        
         // POPULATING SVG_ELEMENTS VECTOR
-
-        // vai ser algo do genero disto i guess
 
         for (XMLElement *child = xml_elem->FirstChildElement(); child != nullptr; child = child->NextSiblingElement())
         {
-            if (strcmp(child->Name(), "ellipse") == 0) {
-                std::cout << "found ellipse" << std::endl;
 
+            // provavelmente era muito melhor se fizesse algo deste genero, provavelmente vou mudar 
+            // // get attributes
+            // std::map<std::string, const XMLAttribute*> attributes;
+            // for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next()) {
+            //     attributes[attr->Name()] = attr;
+            // }
+
+            // ELLIPSE
+            if (strcmp(child->Name(), "ellipse") == 0) {
                 Color color;
                 Point center, radius;
 
-                // for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
+                // 1: cx="100"
+                // 2: cy="100"
+                // 3: rx="95"
+                // 4: ry="20"
+                // 5: fill="red"
 
                 const XMLAttribute *attr = child->FirstAttribute(); // cx
                 const char* cx = attr->Value();
@@ -63,16 +71,87 @@ namespace svg
                 const char* color_str = attr->Value();
                 color = parse_color(color_str);
 
-                // 1: cx="100"
-                // 2: cy="100"
-                // 3: rx="95"
-                // 4: ry="20"
-                // 5: fill="red"
-
                 // isto funciona (amen)
                 Ellipse *elem = new Ellipse(color, center, radius);
                 svg_elements.push_back(elem);
+            }
 
+            // CIRCLE
+            if (strcmp(child->Name(), "circle") == 0) {
+                Color color;
+                Point center;
+                int radius;
+
+                // for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
+
+                // 1: cx="100"
+                // 2: cy="100"
+                // 3: r="95"
+                // 5: fill="red"
+
+                const XMLAttribute *attr = child->FirstAttribute(); // cx
+                const char* cx = attr->Value();
+                int cx_int = stoi(cx);
+
+                attr = attr->Next(); // cy
+                const char* cy = attr->Value();
+                int cy_int = stoi(cy);
+
+                center.x = cx_int;
+                center.y = cy_int;
+
+                attr = attr->Next(); // r
+                const char* radius_str = attr->Value();
+                radius = stoi(radius_str);
+
+                attr = attr->Next(); // fill
+                const char* color_str = attr->Value();
+                color = parse_color(color_str);
+
+                // isto funciona (amen)
+                Circle *elem = new Circle(color, center, radius);
+                svg_elements.push_back(elem);
+            }
+
+            // LINE
+            if (strcmp(child->Name(), "line") == 0) {
+                // <line x1="1" y1="198" x2="1" y2="1" stroke="red"/>
+                Color color;
+
+                // for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
+
+                // 1: x1="1"
+                // 2: y1="198"
+                // 3: x2="1"
+                // 4: y2="1"
+                // 5: stroke="red"
+
+                const XMLAttribute *attr = child->FirstAttribute(); // x1
+                const char* x1 = attr->Value();
+                int x1_int = stoi(x1);
+
+                attr = attr->Next(); // y1
+                const char* y1 = attr->Value();
+                int y1_int = stoi(y1);
+
+                attr = attr->Next(); // x2
+                const char* x2 = attr->Value();
+                int x2_int = stoi(x2);
+
+                attr = attr->Next(); // y2
+                const char* y2 = attr->Value();
+                int y2_int = stoi(y2);
+
+                attr = attr->Next(); // stroke
+                const char* color_str = attr->Value();
+                color = parse_color(color_str);
+
+                Point start = {x1_int,y1_int};
+                Point end = {x2_int,y2_int};
+
+                // isto funciona (amen)
+                Line *elem = new Line(color, start, end);
+                svg_elements.push_back(elem);
             }
         }
     }
